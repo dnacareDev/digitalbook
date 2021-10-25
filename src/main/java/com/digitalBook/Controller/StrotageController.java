6d14cc20b90133ea43be462977ec59a348cf1731
keh6305 <keh6305@gmail.com>
2021-10-26 01:01:18 +0900

    과제, 장소 수정 중

--- src/main/java/com/digitalBook/Controller/StrotageController.java
+++ src/main/java/com/digitalBook/Controller/StrotageController.java
@@ -7,16 +7,15 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/data")
-//기초정보 > 실험장소
-public class StrotageController {
-	
-	//실험장소 목록
-	@GetMapping(value = "/storage")
-	public ModelAndView getStorage(ModelAndView mv) {
-		
+// 기초정보 > 실험장소
+public class StrotageController
+{
+	// 실험장소 목록
+	@RequestMapping(value = "/storage")
+	public ModelAndView getStorage(ModelAndView mv)
+	{
 		mv.setViewName("storage/storage_list");
 		
 		return mv;
 	}
-
 }
