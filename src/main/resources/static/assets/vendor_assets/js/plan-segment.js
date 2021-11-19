ad8a10e1cfcf9550c19f55ae6e20b5da3bbd2407
DESKTOP-2SRB9FU\UserK <jys0323117@naver.com>
2021-11-19 10:12:54 +0900

    color 무한루프 수정

--- src/main/resources/static/assets/vendor_assets/js/plan-segment.js
+++ src/main/resources/static/assets/vendor_assets/js/plan-segment.js
@@ -23,11 +23,7 @@ var segmentEls = document.querySelectorAll('#step4-segment li .segment');
 var segmentMove = document.querySelectorAll('#step4-segment li .arrowMove');
 var segmentRotate = document.querySelectorAll('#step4-segment li .refresh');
 
-var segmentType = 0;
-
-function getDataResult(getDataArr, inner, type){
-	segmentType = type;
-	
+function getDataResult(getDataArr, type){
 	segmentData = getDataArr;
 	tableEls = document.querySelectorAll('.step4-container .table_content');
 	renderSegment(segmentData);
@@ -41,28 +37,13 @@ function onClickColumnBtn(){
 	}
 }
 
-function setColor(){
-	let classReturn = "";
-	if(segmentType === 0){
-	
-	}else if(segmentType === 1){
-	
-	}else if(segmentType === 2){
-	
-	}else if(segmentType === 3){
-	
-	}
-	
-	return classReturn;
-}
-
 function segmentComp(data){
 	var {id, type, repeat} = data;
 	
 	var html = '';
 
 	html += '<li>';
-	html += 	'<div class="segment"  id="segment">' + id + '</div>';
+	html += 	'<div class="segment">' + id + '</div>';
 	html +=     '<div class="arrowMove">';
 	html +=         '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrows-move" viewBox="0 0 16 16">';
 	html +=           '<path fill-rule="evenodd" d="M7.646.146a.5.5 0 0 1 .708 0l2 2a.5.5 0 0 1-.708.708L8.5 1.707V5.5a.5.5 0 0 1-1 0V1.707L6.354 2.854a.5.5 0 1 1-.708-.708l2-2zM8 10a.5.5 0 0 1 .5.5v3.793l1.146-1.147a.5.5 0 0 1 .708.708l-2 2a.5.5 0 0 1-.708 0l-2-2a.5.5 0 0 1 .708-.708L7.5 14.293V10.5A.5.5 0 0 1 8 10zM.146 8.354a.5.5 0 0 1 0-.708l2-2a.5.5 0 1 1 .708.708L1.707 7.5H5.5a.5.5 0 0 1 0 1H1.707l1.147 1.146a.5.5 0 0 1-.708.708l-2-2zM10 8a.5.5 0 0 1 .5-.5h3.793l-1.147-1.146a.5.5 0 0 1 .708-.708l2 2a.5.5 0 0 1 0 .708l-2 2a.5.5 0 0 1-.708-.708L14.293 8.5H10.5A.5.5 0 0 1 10 8z"/>';
@@ -369,6 +350,7 @@ function segmentSetting(data){
 			tableEls[t].addEventListener('click', onClickTableEls);
 		}
 	}
+	
 }
 
 function addWindowEvent(){
