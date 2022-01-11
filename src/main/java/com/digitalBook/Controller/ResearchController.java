928bbbd269056dacde7a548435eee403f585aadd
crinkj <JCrinkey89120@gmail.com>
2022-01-11 11:15:15 +0900

    텍스트 수정

--- src/main/java/com/digitalBook/Controller/ResearchController.java
+++ src/main/java/com/digitalBook/Controller/ResearchController.java
@@ -5,8 +5,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
